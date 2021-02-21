0254bff081d6236f4398a48d6d3162d3266af8ac
User <User@10.1.3.109>
2021-02-21 16:42:41 -0500

    Added the final endpoints, all work

--- restful-bots-app-engine-master/src/main/java/edu/ecu/cs/seng6285/restfulbots/CalcController.java
+++ restful-bots-app-engine-master/src/main/java/edu/ecu/cs/seng6285/restfulbots/CalcController.java
@@ -9,11 +9,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalcController {
 	private static String logClass = "CalcController";
 	private Logger logger = LoggerFactory.getLogger(logClass);
-	
+	 
 	 @GetMapping("/add")
-	 private String add(int a, int b) {
-		 logger.info(String.format("performed calculation of %s", a," and ", b));
-		 return String.format("Answer is %s", (a + b));
+	 private String add(int n, int m) {
+		 logger.info(String.format("performed calculation of %s", (n + " and " + m)));
+		 return String.format("Answer is %s", (n + m));
+	 }
+	 
+	 @GetMapping("/subtract")
+	 private String subtract(int n, int m) {
+		 logger.info(String.format("performed calculation of %s", (n + " and " + m)));
+		 return String.format("Answer is %s", (n - m));
+	 }
+	 
+	 @GetMapping("/times")
+	 private String times(int n, int m) {
+		 logger.info(String.format("perform multiplication of %s", (n + " and " + m )));
+		 return String.format("Answer is %s", (n * m));
+	 }
+	 
+	 @GetMapping("/div")
+	 private String div(int n, int m) {
+		 logger.info(String.format("perform division of %s", (n + " and " + m)));
+		 return String.format("Answer is %s", (n/m));
+	 }
+	 
+	 @GetMapping("/neg")
+	 private String div(int n) {
+		 logger.info(String.format("perform negation of %s", (n)));
+		 return String.format("Answer is %s", (0 - n));
 	 }
 
 }
